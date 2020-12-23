d36e7c32ef0cd25a2c026bc2fed0fdabc4e7ab2b
Phoenix1747 <apertured@public-files.de>
2020-12-23 21:45:23 +0100

    update data

--- autogit.sh
+++ autogit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-git add *
-git commit -m "update data"
-git push
+gitpath="$(dirname $(realpath $0))"
+git -C $gitpath add "$gitpath/*"
+git -C $gitpath commit -m "update data"
+git -C $gitpath push
