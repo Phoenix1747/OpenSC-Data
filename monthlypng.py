4c7def5ad592adedea0ec34f290cb6da5f0482f6
Phoenix1747 <apertured@public-files.de>
2020-12-23 16:34:52 +0100

    add stuff

---
+++ monthlypng.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+##
+## Monatlicher Verlauf des täglichen cps Durchschnitts.
+##
+
+import matplotlib.pyplot as plt
+import time
+import csv
+from datetime import datetime
+from datetime import date
+import calendar
+import matplotlib.dates as mdates
+
+files = []
+
+year = datetime.now().year
+month = datetime.now().month
+num_days = calendar.monthrange(year, month)[1]
+days = [date(year, month, day) for day in range(1, num_days+1)]
+
+for day in days:
+	files.append(day.strftime("%Y%m%d"))
+
+plotfile = "docs/images/monthly.png" #str(year) + str(month) + ".png"
+
+x = []
+y = []
+count = 0
+
+for daystr in files:
+	filename = "data/" + daystr + ".csv"
+	try:
+		with open(filename,newline="") as file:
+			avg = 0
+			mcount = 0
+
+			csvread = csv.reader(file, delimiter=';')
+			for row in csvread:
+				avg += float(row[1])
+				mcount += 1
+
+			avg /= mcount
+			x.append(datetime.strptime(daystr,"%Y%m%d"))
+			y.append(round(avg,2))
+		count += 1
+	except:
+		continue
+		#just continue with the days that exist
+
+tolu = time.strftime('%Y-%m-%d',time.localtime()) #time of last update
+substring = "Daily average CPS month of " + calendar.month_name[month] + " " + str(year)
+
+#plt.style.use("seaborn")
+
+plt.plot_date(x,y, linestyle="solid", linewidth=1.1, marker=",", color="orange")
+
+plt.title("Last update: " + tolu, fontsize=9, loc="center")
+plt.suptitle(substring, ha="center")
+plt.xlabel("Day Of Month")
+plt.ylabel("Average CPS")
+plt.grid()
+#plt.ylim(10.0,20.0)
+
+date_format = mdates.DateFormatter("%d")
+plt.gca().xaxis.set_major_formatter(date_format)
+plt.gcf().autofmt_xdate()
+
+plt.savefig(plotfile)
+
+#plt.show()
---
+++ rserial.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import serial
+import time
+import csv
+import os
+
+ser = serial.Serial("/dev/ttyACM0", 2000000)
+ser.flushInput()
+
+while True:
+	try:
+		ser_bytes = ser.readline()
+
+		filename = "data/" + time.strftime('%Y%m%d',time.localtime()) + ".csv"
+		timestr = time.strftime('%H%M',time.localtime())
+
+		data = float(ser_bytes.decode("utf-8"))
+
+		with open(filename,"a",newline="") as file:
+			writer = csv.writer(file, delimiter=";")
+			writer.writerow([timestr,data])
+
+		os.system("python3 dailypng.py &")
+		os.system("python3 monthlypng.py &")
+	except:
+		continue
+
