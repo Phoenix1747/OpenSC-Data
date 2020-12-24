782908be3fb9dced7c39b82389fc4f6a78b3f283
Phoenix1747 <apertured@public-files.de>
2020-12-24 21:29:45 +0100

    small autogit fixes

--- autogit.sh
+++ autogit.sh
@@ -3,6 +3,6 @@
 gitpath="$(dirname $(realpath $0))"
 
 git -C $gitpath pull
-git -C $gitpath add "$gitpath/docs" "$gitpath/data"
-git -C $gitpath commit -m "update data"
+git -C $gitpath add "$gitpath/docs" "$gitpath/data/*.csv"
+git -C $gitpath commit -m "auto data update"
 git -C $gitpath push
