8fb81a2563d57ef50df87684e6146b9c70fea402
Phoenix1747 <apertured@public-files.de>
2020-12-25 16:31:49 +0100

    small misc changes

--- rserial.py
+++ rserial.py
@@ -13,35 +13,31 @@ ser = serial.Serial("/dev/ttyACM0", 2000000)
 ser.flushInput()
 
 fullpath = os.path.dirname(os.path.abspath(__file__)) + "/"
-
 indexfile = fullpath + "docs/index.html"
 
-while True:
-	#try:
-		ser_bytes = ser.readline()
+while True: #Just keep running forever
+	ser_bytes = ser.readline() #read serial data
 
-		filename = fullpath + "data/" + time.strftime('%Y%m%d',time.localtime()) + ".csv"
-		timestr = time.strftime('%H%M',time.localtime())
+	filename = fullpath + "data/" + time.strftime('%Y%m%d',time.localtime()) + ".csv" #write data to yymmdd.csv
+	timestr = time.strftime('%H%M',time.localtime())
 
-		data = float(ser_bytes.decode("utf-8"))
+	data = float(ser_bytes.decode("utf-8"))
 
-		with open(filename,"a",newline="") as file:
-			writer = csv.writer(file, delimiter=",")
-			writer.writerow([timestr,data])
+	with open(filename,"a",newline="") as file: #save as csv to file
+		writer = csv.writer(file, delimiter=",")
+		writer.writerow([timestr,data])
 
-		pnggen.dailypng() #generate pngs for webpage
-		pnggen.monthlypng()
+	pnggen.dailypng() #generate pngs for webpage
+	pnggen.monthlypng()
 
-		indexrepl = {
-			"$current" : str(data),
-			"$lastup" : time.strftime("%Y-%m-%d, %H:%M",time.localtime())
-			} #holds all values to be replaced in web
+	indexrepl = {
+		"$current" : str(data),
+		"$lastup" : time.strftime("%Y-%m-%d, %H:%M",time.localtime())
+		} #holds all values to be replaced for the website
 
-		with open(indexfile + ".orig", "r") as file:
-			content = file.read() #read whole file to string
-			for key in indexrepl: #replace all values
-				content = content.replace(key, indexrepl[key])
-			with open(indexfile, "w") as f: #save file with new values
-				f.write(content)
-	#except:
-	#	continue
+	with open(indexfile + ".orig", "r") as file:
+		content = file.read() #read whole file to string
+		for key in indexrepl: #replace all values
+			content = content.replace(key, indexrepl[key])
+		with open(indexfile, "w") as f: #save file with new values
+			f.write(content)
